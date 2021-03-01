a6849feae0588ae95dfc5a47d002cf79371b5840
Thiago Dias <thiagohd93@gmail.com>
2021-03-01 13:34:41 -0300

    Header/Rotas

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
 import React from "react";
-import Api from "./api/Api";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 import "./App.css";
+import Header from "./Components/Header";
+import Home from "./Components/Home";
+import Footer from "./Components/Footer";
+import Login from "./Components/Login/Login";
 
 const App = () => {
   return (
-    <h1>
-      <Api />
-    </h1>
+    <BrowserRouter>
+      <Header />
+      <Switch>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/login" element={<Login />}></Route>
+      </Switch>
+      <Footer />
+    </BrowserRouter>
   );
 };
 
